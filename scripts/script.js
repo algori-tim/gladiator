cdae2caecbe37fce77387f776c1f3fb1ebd27970
Tim Johnson <timaj88@gmail.com>
2023-10-11 21:05:24 -0500

    add gh action

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
 var canvas = document.getElementById("myCanvas");
 var canvasContext = canvas.getContext("2d");
-            
+
 var enemySprite = new Image();
 enemySprite.src = "images/enemySpriteSheet.png";
 
 var playerSpriteSheet = new Image();
 playerSpriteSheet.src = "images/playerSpriteSheet.png";
-            
+
 var text = "";
 
 var dummyDead = false;
@@ -41,65 +41,67 @@ function draw() {
 
   playerObject.centerX = playerObject.x + 23;
   playerObject.centerY = playerObject.y + 23;
-  
+
   //clears canvas on each frame
   canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-  
+
   checkPlayerBounderies();
 
-  if(enemyCenterY < playerObject.centerY){
+  if (enemyCenterY < playerObject.centerY) {
     drawEnemy();
     drawPlayer();
-  }
-  else{
+  } else {
     drawPlayer();
     drawEnemy();
   }
 
   drawStats();
-  
-  if(playerObject.isAttacking){  
-    playerObject.stillAttacking = true;
-    playerAnimationCounter += .5;
 
-    if (playerAnimationCounter > 4.5){
+  if (playerObject.isAttacking) {
+    playerObject.stillAttacking = true;
+    playerAnimationCounter += 0.5;
+
+    if (playerAnimationCounter > 4.5) {
       checkForPlayerHitEnemy();
     }
 
-    if(playerAnimationCounter > attackStopCount){
+    if (playerAnimationCounter > attackStopCount) {
       playerObject.isAttacking = false;
       enemyObject.takingDamageCounter = 0;
       playerObject.stillAttacking = false;
     }
   }
-  
-  if(dummyDead){
-    
+
+  if (dummyDead) {
     enemyObject.health = 20;
     enemyObject.x = 250 - 23;
     enemyObject.y = 40 - 23;
-    
+
     dummyDead = false;
     dummiesKilled += 1;
-    playerObject.health += .5;
+    playerObject.health += 0.5;
   }
-  
+
   checkForEnemyAttackInRange();
 
-  if(enemyObject.attackInRange && enemyObject.isAttacking == false && enemyAttackPointCounter > enemyAttackPointSetter){
+  if (
+    enemyObject.attackInRange &&
+    enemyObject.isAttacking == false &&
+    enemyAttackPointCounter > enemyAttackPointSetter
+  ) {
     enemyAnimationCounter = 0;
     enemyAttack();
   }
-  
-  if(enemyObject.isAttacking){  
-    enemyAnimationCounter += .35;
+
+  if (enemyObject.isAttacking) {
+    enemyAnimationCounter += 0.35;
     enemyObject.attackCounter += 1;
-    
-    if (enemyAnimationCounter > 4.5){
+
+    if (enemyAnimationCounter > 4.5) {
       checkForEnemyHitPlayer();
     }
-    
-    if(enemyAnimationCounter > attackStopCount){
+
+    if (enemyAnimationCounter > attackStopCount) {
       enemyAttackPointCounter = 0;
       enemyAttackPointSetter = Math.floor(Math.random() * 15);
       enemyObject.isAttacking = false;
@@ -108,52 +110,67 @@ function draw() {
       playerObject.takingDamageCounter = 0;
     }
   }
-  
-  if(playerObject.health < 1){
+
+  if (playerObject.health < 1) {
+    console.log("DEAD");
+    alert(
+      "You seem to have died... You have slain " + dummiesKilled + " warriors!"
+    );
     playerObject.health = 1;
-    alert("You seem to have died... You have slain " + dummiesKilled + " warriors!");
     document.location.href = "index.html";
   }
 
-  document.getElementById("dummyHealth").innerHTML = "Dummy Health: " + enemyObject.health;
-  document.getElementById("playerHealth").innerHTML = "Player Health: " + playerObject.health;
-  document.getElementById("dummiesKilled").innerHTML = "Dummies Killed " + dummiesKilled;
+  //DEBUG code
+  // document.getElementById("dummyHealth").innerHTML =
+  //   "Dummy Health: " + enemyObject.health;
+  // document.getElementById("playerHealth").innerHTML =
+  //   "Player Health: " + playerObject.health;
+  // document.getElementById("dummiesKilled").innerHTML =
+  //   "Dummies Killed " + dummiesKilled;
 
-  if(enemyObject.isLookingUp) text = "Up"
-  if(enemyObject.isLookingDown) text = "Down"
-  if(enemyObject.isLookingLeft) text = "Left"
-  if(enemyObject.isLookingRight) text = "Right"
-  
-  document.getElementById("text").innerHTML = "Enemy looking..." + text;
-  document.getElementById("enemyX").innerHTML = "Enemy X: " + Math.floor(enemyCenterX);
-  document.getElementById("enemyY").innerHTML = "Enemy Y: " + Math.floor(enemyCenterY);
-  document.getElementById("playerX").innerHTML = "Player X: " + Math.floor(playerObject.centerX);
-  document.getElementById("playerY").innerHTML = "Player Y: " + Math.floor(playerObject.centerY);
-  document.getElementById("attackSetter").innerHTML = "AttackSetter: " + enemyAttackPointSetter;
+  // if (enemyObject.isLookingUp) text = "Up";
+  // if (enemyObject.isLookingDown) text = "Down";
+  // if (enemyObject.isLookingLeft) text = "Left";
+  // if (enemyObject.isLookingRight) text = "Right";
 
-}//end of game loop draw()
+  // document.getElementById("text").innerHTML = "Enemy looking..." + text;
+  // document.getElementById("enemyX").innerHTML =
+  //   "Enemy X: " + Math.floor(enemyCenterX);
+  // document.getElementById("enemyY").innerHTML =
+  //   "Enemy Y: " + Math.floor(enemyCenterY);
+  // document.getElementById("playerX").innerHTML =
+  //   "Player X: " + Math.floor(playerObject.centerX);
+  // document.getElementById("playerY").innerHTML =
+  //   "Player Y: " + Math.floor(playerObject.centerY);
+  // document.getElementById("attackSetter").innerHTML =
+  //   "AttackSetter: " + enemyAttackPointSetter;
+} //end of game loop draw()
 
 //drawing health bars and kill count
 function drawStats() {
-  
   canvasContext.beginPath();
-  canvasContext.font="25px Times";
-  canvasContext.fillText("Your Health...", 15, 32)
+  canvasContext.font = "25px Times";
+  canvasContext.fillText("Your Health...", 15, 32);
   canvasContext.stroke();
 
   canvasContext.beginPath();
-  canvasContext.font="25px Times";
-  canvasContext.fillText("Kills..." + dummiesKilled, 385, 32)
+  canvasContext.font = "25px Times";
+  canvasContext.fillText("Kills..." + dummiesKilled, 385, 32);
   canvasContext.stroke();
 
   canvasContext.beginPath();
-  canvasContext.rect(20, 35, 125, 10)
+  canvasContext.rect(20, 35, 125, 10);
   canvasContext.fillRect(20, 35, playerObject.health * 5, 10);
   canvasContext.stroke();
 
   canvasContext.beginPath();
-  canvasContext.fillStyle="red";
-  canvasContext.fillRect(enemyObject.healthBarX + 13, enemyObject.healthBarY, enemyObject.health, 6);
+  canvasContext.fillStyle = "red";
+  canvasContext.fillRect(
+    enemyObject.healthBarX + 13,
+    enemyObject.healthBarY,
+    enemyObject.health,
+    6
+  );
   canvasContext.stroke();
 }
 
@@ -164,55 +181,54 @@ document.addEventListener("keyup", keyUpHandler, false);
 //event handler for pressing a key
 function keyDownHandler(e) {
   //player holding up key
-  if(e.keyCode == 38){
+  if (e.keyCode == 38) {
     playerObject.isMovingUp = true;
     playerSpriteFrameY = 1;
 
     playerLookingUp(playerObject);
   }
   //player holding down key
-  if(e.keyCode == 40){
+  if (e.keyCode == 40) {
     playerObject.isMovingDown = true;
     playerSpriteFrameY = 0;
 
     playerLookingDown(playerObject);
   }
   //player holding left key
-  if(e.keyCode == 37){
+  if (e.keyCode == 37) {
     playerObject.isMovingLeft = true;
     playerSpriteFrameY = 3;
-    
+
     playerLookingLeft(playerObject);
   }
   //player holding right key
-  if(e.keyCode == 39){
+  if (e.keyCode == 39) {
     playerObject.isMovingRight = true;
     playerSpriteFrameY = 2;
-    
+
     playerLookingRight(playerObject);
   }
 
   //player press space bar
-  if(e.keyCode == 32){
+  if (e.keyCode == 32) {
     playerObject.isAttacking = true;
-    
-    if(!playerObject.stillAttacking){
 
+    if (!playerObject.stillAttacking) {
       playerAnimationCounter = 0;
-      
-      if(playerObject.isLookingUp){
+
+      if (playerObject.isLookingUp) {
         playerSpriteFrameY = 5;
       }
-      
-      if(playerObject.isLookingDown){
+
+      if (playerObject.isLookingDown) {
         playerSpriteFrameY = 4;
       }
-      
-      if(playerObject.isLookingLeft){
+
+      if (playerObject.isLookingLeft) {
         playerSpriteFrameY = 7;
       }
-      
-      if(playerObject.isLookingRight){
+
+      if (playerObject.isLookingRight) {
         playerSpriteFrameY = 6;
       }
     }
@@ -222,23 +238,23 @@ function keyDownHandler(e) {
 //event handler for releasing a key
 function keyUpHandler(e) {
   //player release up key
-  if(e.keyCode == 38){
+  if (e.keyCode == 38) {
     playerObject.isMovingUp = false;
   }
   //player release down key
-  if(e.keyCode == 40){
+  if (e.keyCode == 40) {
     playerObject.isMovingDown = false;
   }
   //player release left key
-  if(e.keyCode == 37){
+  if (e.keyCode == 37) {
     playerObject.isMovingLeft = false;
   }
   //player release right key
-  if(e.keyCode == 39){
+  if (e.keyCode == 39) {
     playerObject.isMovingRight = false;
   }
 
-  if(e.keyCode == 32){
+  if (e.keyCode == 32) {
     // attack = false;
   }
 }
